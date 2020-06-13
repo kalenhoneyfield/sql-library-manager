241c4c6870523c7d5d76abd551c4ecd1420de665
Kalen <kalenhoneyfield@gmail.com>
2020-06-13 18:58:28 -0400

    I have no idea what is going on at this point... nothing is changing

---
+++ routes/books.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const router = express.Router();
+const Book = require('../models').Book;
+
+//catch server side errors in one place
+function asyncHandler(callback) {
+  return async (req, res, next) => {
+    try {
+      await callback(req, res, next);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  };
+}
+
+/* GET all books listing. */
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const books = await Book.findAll({
+      order: [['year', 'DESC']],
+    });
+    res.render('index', { books: books, title: 'Books' });
+  })
+);
+
+/* Create a new book form. */
+router.get('/new', (req, res) => {
+  res.render('new-book', { books: {}, title: 'New Book' });
+});
+
+/* POST new book */
+router.post(
+  '/new',
+  asyncHandler(async (req, res) => {
+    console.log(req.body);
+    let book;
+    try {
+      book = await Book.create(req.body);
+      res.redirect('/books/' + book.id);
+    } catch (err) {
+      if (err.name === 'SequelizeValidationError') {
+        book = await Book.build(req.body);
+        res.render('new-book', {
+          book: book,
+          errors: err.errors,
+          title: 'New Book With Errors?',
+        });
+      } else {
+        throw err;
+      }
+    }
+  })
+);
+
+/* GET a book */
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      res.render('view-book', { book: book, title: book.title });
+    } else {
+      res.sendStatus(404);
+    }
+  })
+);
+
+/* Edit specific book form. */
+router.get(
+  '/:id/edit',
+  asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      res.render('update-book', { book: book, title: `Edit: ${book.title}` });
+    } else {
+      res.sendStatus(404);
+    }
+  })
+);
+
+/* Rubric asks for /:id during refactor this post route should go back to /:id/edit */
+/* Update a single book */
+router.post(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    let book;
+    try {
+      book = await Book.findByPk(req.params.id);
+      if (book) {
+        await book.update(req.body);
+        res.redirect('/books/' + book.id);
+      } else {
+        res.sendStatus(404);
+      }
+    } catch (err) {
+      if (err.name === 'SequelizeValidationError') {
+        book = await Book.build(req.body);
+        book.id = req.params.id;
+        res.render('update-book', {
+          book: book,
+          errors: err.errors,
+          title: `Edit: Oops looks like we're missing something`,
+        });
+      } else {
+        throw err;
+      }
+    }
+  })
+);
+
+/* Using a route here saves us the hassle of setting up some cute javascript confirmation modal may revisit later */
+/* Delete book */
+router.get(
+  '/:id/delete',
+  asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      res.render('delete-book', { book: book, title: `Delete "${book.title}"?` });
+    } else {
+      res.sendStatus(404);
+    }
+  })
+);
+
+/* Delete a book */
+router.post(
+  '/:id/delete',
+  asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      await book.destroy();
+      res.redirect('/books');
+    } else {
+      res.sendStatus(404);
+    }
+  })
+);
+
+module.exports = router;
